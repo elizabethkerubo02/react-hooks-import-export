eca649902925dc32a5b952bdc7a23f55e8f9ff57
elizabethkerubo02 <keruboelizabeth02@gmail.com>
2023-04-20 13:03:55 +0300

    learned about export & import keywords

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,14 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVede from "./parks/MesaVerde"
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
+  MesaVede();
 }
+
+
+
+  export default ColoradoStateParks;
